9c85464a56a76ff3669b9221bfc2fc351d14a060
ashirwadkhatri <khatriashirwad890@gmail.com>
2025-08-24 14:57:14 +0000

    added low triangular matrix

---
+++ assignment 2/5.2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of lower triangular matrix: ";
+    cin >> n;
+
+    // Only store the non-zero elements
+    int size = n * (n + 1) / 2; // number of elements in lower triangle so that only neccessary is stored like in n=4 insted of 16 only 10 elements are stored to save space
+    int arr[size];
+
+    cout << "Enter " << size << " elements for the lower triangle:\n";
+    for (int i = 0; i < size; i++) cin >> arr[i];
+
+    int k = 0; // index in arr[]
+    cout << "Lower Triangular Matrix:\n";
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i >= j) {
+                cout << arr[k++] << " "; // k starts from 0 then when condition is met it increases since we sote it in 1 d aray at first and k access that 1 d array from arr[0] to arr[n] 
+            } else {
+                cout << "0 ";           // above main diagonal
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
