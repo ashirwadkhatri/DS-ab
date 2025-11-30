2637cf74a74516393270f56e9bf17cb04912983b
ashirwad khatri <khatriashirwad890@gmail.com>
2025-11-30 23:02:01 +0530

    Update 4.cpp

--- assignment5/4.cpp
+++ assignment5/4.cpp
@@ -1,72 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-// Structure for a linked list node
-struct Node {
+// Node class (no struct)
+class Node {
+public:
     int data;
-    struct Node* next;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
 };
 
-// Function to create a new node
-struct Node* newNode(int data) {
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = data;
-    temp->next = NULL;
-    return temp;
-}
-
 // Function to append a node at the end
-void append(struct Node** head_ref, int data) {
-    struct Node* temp = newNode(data);
-    if (*head_ref == NULL) {
-        *head_ref = temp;
+void append(Node*& head, int data) {
+    Node* temp = new Node(data);
+
+    if (head == nullptr) {
+        head = temp;
         return;
     }
-    struct Node* last = *head_ref;
-    while (last->next != NULL)
+
+    Node* last = head;
+    while (last->next != nullptr)
         last = last->next;
+
     last->next = temp;
 }
 
 // Function to print the linked list
-void printList(struct Node* head) {
-    while (head != NULL) {
-        printf("%d->", head->data);
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << "->";
         head = head->next;
     }
-    printf("NULL\n");
+    cout << "NULL\n";
 }
 
 // Function to reverse the linked list
-struct Node* reverseList(struct Node* head) {
-    struct Node *prev = NULL, *curr = head, *next = NULL;
+Node* reverseList(Node* head) {
+    Node* prev = nullptr;
+    Node* curr = head;
+    Node* next = nullptr;
 
-    while (curr != NULL) {
-        next = curr->next;   // store next node
-        curr->next = prev;   // reverse link
-        prev = curr;         // move prev ahead
-        curr = next;         // move curr ahead
+    while (curr != nullptr) {
+        next = curr->next;   // Store next node
+        curr->next = prev;   // Reverse the link
+        prev = curr;         // Move prev
+        curr = next;         // Move curr
     }
 
-    return prev; // new head of the reversed list
+    return prev; // New head of reversed list
 }
 
 // Main function
 int main() {
-    struct Node* head = NULL;
+    Node* head = nullptr;
 
     // Creating linked list: 1->2->3->4->NULL
-    append(&head, 1);
-    append(&head, 2);
-    append(&head, 3);
-    append(&head, 4);
+    append(head, 1);
+    append(head, 2);
+    append(head, 3);
+    append(head, 4);
 
-    printf("Original List: ");
+    cout << "Original List: ";
     printList(head);
 
     head = reverseList(head);
 
-    printf("Reversed List: ");
+    cout << "Reversed List: ";
     printList(head);
 
     return 0;
