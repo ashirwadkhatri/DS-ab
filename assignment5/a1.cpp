8bce751f7155a97b51b9c5d98dc62e3c4024bf2a
ashirwad khatri <khatriashirwad890@gmail.com>
2025-11-30 23:09:01 +0530

    Refactor linked list implementation to C++

--- assignment5/a1.cpp
+++ assignment5/a1.cpp
@@ -1,71 +1,69 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-// Structure for a node
-struct Node {
+// Node class
+class Node {
+public:
     int data;
-    struct Node* next;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
 };
 
-// Function to create a new node
-struct Node* newNode(int data) {
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = data;
-    temp->next = NULL;
-    return temp;
-}
-
 // Function to find intersection point
-struct Node* getIntersectionNode(struct Node* headA, struct Node* headB) {
+Node* getIntersectionNode(Node* headA, Node* headB) {
     if (!headA || !headB)
-        return NULL;
+        return nullptr;
 
-    struct Node *a = headA, *b = headB;
+    Node *a = headA, *b = headB;
 
-    // Loop until both pointers meet or both become NULL
+    // Loop until both pointers meet or become NULL
     while (a != b) {
-        a = (a == NULL) ? headB : a->next;
-        b = (b == NULL) ? headA : b->next;
+        a = (a == nullptr) ? headB : a->next;
+        b = (b == nullptr) ? headA : b->next;
     }
 
-    return a; // Either intersection node or NULL
+    return a;  // Intersection node OR nullptr
 }
 
-// Helper function to print list
-void printList(struct Node* head) {
-    while (head) {
-        printf("%d", head->data);
+// Function to print list
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data;
         if (head->next)
-            printf("->");
+            cout << "->";
         head = head->next;
     }
-    printf("\n");
+    cout << endl;
 }
 
-// Driver code
+// Main function
 int main() {
-    // Create two linked lists
-    // A: 4->1->8->4->5
-    // B: 5->6->1->8->4->5
-    struct Node* newNode8 = newNode(8);
-    newNode8->next = newNode(4);
-    newNode8->next->next = newNode(5);
+    // Create merged node sequence: 8->4->5
+    Node* newNode8 = new Node(8);
+    newNode8->next = new Node(4);
+    newNode8->next->next = new Node(5);
 
-    struct Node* headA = newNode(4);
-    headA->next = newNode(1);
-    headA->next->next = newNode8; // merge point
+    // List A: 4->1->(8->4->5)
+    Node* headA = new Node(4);
+    headA->next = new Node(1);
+    headA->next->next = newNode8;
 
-    struct Node* headB = newNode(5);
-    headB->next = newNode(6);
-    headB->next->next = newNode(1);
-    headB->next->next->next = newNode8; // merge point
+    // List B: 5->6->1->(8->4->5)
+    Node* headB = new Node(5);
+    headB->next = new Node(6);
+    headB->next->next = new Node(1);
+    headB->next->next->next = newNode8;
 
-    struct Node* intersection = getIntersectionNode(headA, headB);
+    Node* intersection = getIntersectionNode(headA, headB);
 
     if (intersection)
-        printf("Intersection at node with data = %d\n", intersection->data);
+        cout << "Intersection at node with data = " << intersection->data << endl;
     else
-        printf("No intersection found\n");
+        cout << "No intersection found\n";
 
     return 0;
 }
