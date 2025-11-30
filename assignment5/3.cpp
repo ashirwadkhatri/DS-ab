d9974c962c1a67a4d2d3cd819d7ea085b59919b6
ashirwad khatri <khatriashirwad890@gmail.com>
2025-11-30 22:59:22 +0530

    Refactor linked list implementation to use classes

--- assignment5/3.cpp
+++ assignment5/3.cpp
@@ -1,69 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-// Structure for a linked list node
-struct Node {
+// Node class (no struct used)
+class Node {
+public:
     int data;
-    struct Node* next;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
 };
 
-// Function to create a new node
-struct Node* newNode(int data) {
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = data;
-    temp->next = NULL;
-    return temp;
-}
-
 // Function to append a node at the end
-void append(struct Node** head_ref, int data) {
-    struct Node* temp = newNode(data);
-    if (*head_ref == NULL) {
-        *head_ref = temp;
+void append(Node*& head, int data) {
+    Node* temp = new Node(data);
+
+    if (head == nullptr) {
+        head = temp;
         return;
     }
-    struct Node* last = *head_ref;
-    while (last->next != NULL)
+
+    Node* last = head;
+    while (last->next != nullptr)
         last = last->next;
+
     last->next = temp;
 }
 
 // Function to print linked list
-void printList(struct Node* head) {
-    while (head != NULL) {
-        printf("%d", head->data);
-        if (head->next != NULL)
-            printf("->");
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data;
+        if (head->next != nullptr)
+            cout << "->";
         head = head->next;
     }
-    printf("\n");
+    cout << endl;
 }
 
 // Function to find and print the middle element
-void findMiddle(struct Node* head) {
-    struct Node *slow = head, *fast = head;
+void findMiddle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
 
-    // Move fast by 2 steps and slow by 1 step
-    while (fast != NULL && fast->next != NULL) {
+    // Move fast 2 steps, slow 1 step
+    while (fast != nullptr && fast->next != nullptr) {
         slow = slow->next;
         fast = fast->next->next;
     }
 
-    printf("Middle element: %d\n", slow->data);
+    cout << "Middle element: " << slow->data << endl;
 }
 
-// Main function
+// Main
 int main() {
-    struct Node* head = NULL;
+    Node* head = nullptr;
 
-    // Creating linked list: 1->2->3->4->5
-    append(&head, 1);
-    append(&head, 2);
-    append(&head, 3);
-    append(&head, 4);
-    append(&head, 5);
+    // Create linked list: 1->2->3->4->5
+    append(head, 1);
+    append(head, 2);
+    append(head, 3);
+    append(head, 4);
+    append(head, 5);
 
-    printf("Linked List: ");
+    cout << "Linked List: ";
     printList(head);
 
     findMiddle(head);
